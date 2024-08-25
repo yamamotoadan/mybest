4e352cac35b1d7132763a7edc9dba8fc678d8f53
adan <ec1158sv@ed.ritsumei.ac.jp>
2024-08-26 00:02:25 +0900

    fix babel error

--- babel.config.js
+++ babel.config.js
@@ -54,13 +54,13 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-transform-private-methods'
+        '@babel/plugin-transform-private-methods',
         {
           loose: true
         }
       ],
       [
-        '@babel/plugin-transform-private-property-in-object'
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
