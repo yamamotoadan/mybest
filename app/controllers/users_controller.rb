029b1e44032235e5418404e940981a5cf2b8d51b
adan <ec1158sv@ed.ritsumei.ac.jp>
2024-09-24 15:12:56 +0900

    あ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id]) 
-    end
-    def iine
+  def show
       @user = User.find(params[:id]) 
-      @tweets = Tweet.all
-    end
+  end
+  def iine
+    @user = User.find(params[:id]) 
+    @tweets = Tweet.all
+  end
+  def mypage
+    @user = current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'users/show/iine/:id', to: 'users#iine'
-  resources :users, only: [:show, :iine]
+  resources :users, only: [:show, :iine, :mypage]
   resources :tweets do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
