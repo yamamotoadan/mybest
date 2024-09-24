d094b84c8aa2df2ac9a6c79feaef2b6238b58c60
adan <ec1158sv@ed.ritsumei.ac.jp>
2024-09-24 15:43:19 +0900

    あ

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'users/show/iine/:id', to: 'users#iine'
-  resources :users, only: [:show, :iine, :mypage]
+  
+  get 'users/show/iine/:id', to: 'users#iine', as: 'user_iine'
+  get 'users/mypage/:id', to: 'users#mypage', as: 'user_mypage'
+
+  resources :users, only: [:show]
+
   resources :tweets do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy]
+    
     member do
       get 'next', to: 'tweets#next'
       get 'prev', to: 'tweets#prev'
     end
   end
+
   root 'tweets#index'
 end
